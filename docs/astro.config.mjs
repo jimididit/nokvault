e84983a8dfb687fe5c2fc1bcbd31261fd70cdc6a
jimididit <jimi@jimididit.com>
2026-01-16 21:48:03 -0500

    add: SEO integration, custom icons

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(), 
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ],
   site: 'https://jimididit.github.io',
   base: '/nokvault/',
   output: 'static',
